1d278c113454799f7803f58b8a799732ff0c2f84
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2023-11-06 15:07:01 +0000

    Vectorised loop 3 - 1744ms

---
+++ COMP3001 CW1 Q2/COMP3001 CW1 Q2/q2.cpp
@@ -0,0 +1,190 @@
+/*
+------------------DR VASILIOS KELEFOURAS-----------------------------------------------------
+------------------COMP3001 ------------------------------------------------------------------
+------------------COMPUTER SYSTEMS MODULE-------------------------------------------------
+------------------UNIVERSITY OF PLYMOUTH, SCHOOL OF ENGINEERING, COMPUTING AND MATHEMATICS---
+*/
+
+#include <stdio.h> //this library is needed for printf function
+#include <stdlib.h> //this library is needed for rand() function
+#include <windows.h> //this library is needed for pause() function
+#include <time.h> //this library is needed for clock() function
+#include <math.h> //this library is needed for abs()
+#include <pmmintrin.h>
+#include <process.h>
+//#include <chrono>
+#include <iostream>
+#include <immintrin.h>
+
+void initialize();
+void initialize_again();
+void slow_routine(float alpha, float beta);//you will optimize this routine
+unsigned short int Compare(float alpha, float beta);
+unsigned short int equal(float const a, float const b);
+
+#define N 8192 //input size
+__declspec(align(64)) float A[N][N], u1[N], u2[N], v1[N], v2[N], x[N], y[N], w[N], z[N], test[N];
+
+#define TIMES_TO_RUN 1 //how many times the function will run
+#define EPSILON 0.0001
+
+int main() {
+
+	float alpha = 0.23f, beta = 0.45f;
+
+	//define the timers measuring execution time
+	clock_t start_1, end_1; //ignore this for  now
+
+	initialize();
+
+	start_1 = clock(); //start the timer 
+
+	for (int i = 0; i < TIMES_TO_RUN; i++)//this loop is needed to get an accurate ex.time value
+		slow_routine(alpha, beta);
+
+
+	end_1 = clock(); //end the timer 
+
+	printf(" clock() method: %ldms\n", (end_1 - start_1) / (CLOCKS_PER_SEC / 1000));//print the ex.time
+
+	if (Compare(alpha, beta) == 0)
+		printf("\nCorrect Result\n");
+	else
+		printf("\nINcorrect Result\n");
+	float FLOPS = (12 * N ^ 2 + 2 * N);
+	printf("\n%f FLOPS achieved\n", FLOPS);
+	system("pause"); //this command does not let the output window to close
+	
+	return 0; //normally, by returning zero, we mean that the program ended successfully. 
+}
+
+
+void initialize() {
+
+	unsigned int    i, j;
+
+	//initialization
+	for (i = 0;i < N;i++)
+		for (j = 0;j < N;j++) {
+			A[i][j] = (i % 21) + (j % 32) - 0.012f;
+
+		}
+
+	for (i = 0;i < N;i++) {
+		z[i] = (i % 9) * 0.8f;
+		x[i] = 0.1f;
+		u1[i] = (i % 9) * 0.2f;
+		u2[i] = (i % 9) * 0.3f;
+		v1[i] = (i % 9) * 0.4f;
+		v2[i] = (i % 9) * 0.5f;
+		w[i] = 0.0f;
+		y[i] = (i % 9) * 0.7f;
+	}
+
+}
+
+void initialize_again() {
+
+	unsigned int    i, j;
+
+	//initialization
+	for (i = 0;i < N;i++)
+		for (j = 0;j < N;j++) {
+			A[i][j] = (i % 21) + (j % 32) - 0.012f;
+
+		}
+
+	for (i = 0;i < N;i++) {
+		z[i] = (i % 9) * 0.8f;
+		x[i] = 0.1f;
+		test[i] = 0.0f;
+		u1[i] = (i % 9) * 0.2f;
+		u2[i] = (i % 9) * 0.3f;
+		v1[i] = (i % 9) * 0.4f;
+		v2[i] = (i % 9) * 0.5f;
+		y[i] = (i % 9) * 0.7f;
+	}
+
+}
+
+//you will optimize this routine
+void slow_routine(float alpha, float beta) {
+
+	unsigned int i, j;
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			A[i][j] += alpha * u1[i] * v1[j] + u2[i] * v2[j];
+
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			x[i] += A[j][i] * y[j] + beta;
+
+	__m256 ymm0, ymm1, ymm2, ymm3;
+	ymm0 = _mm256_set_ps(3.22f, 3.22f, 3.22f, 3.22f, 3.22f, 3.22f, 3.22f, 3.22f); // set num values
+
+	for (i = 0; i < N; i += 8)
+		ymm3 = _mm256_load_ps(&z[i]); // load 8 elements of z[]
+		ymm1 = _mm256_load_ps(&x[i]); // load 8 elements of x[]
+		_mm256_fmadd_ps(ymm0, ymm3, ymm0); // multiply ymm0 and ymm3, store result into ymm0
+		ymm2 = _mm256_add_ps(ymm1, ymm0); // add ymm1 and ymm0, store results into ymm2
+		_mm256_store_ps(&x[i], ymm2); // store values of ymm2 into x[]
+
+
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			w[i] += alpha * A[i][j] * x[j] + beta;
+
+
+}
+
+
+unsigned short int Compare(float alpha, float beta) {
+
+	unsigned int i, j;
+
+	initialize_again();
+
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			A[i][j] += alpha * u1[i] * v1[j] + u2[i] * v2[j];
+
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			x[i] += A[j][i] * y[j] + beta;
+
+	for (i = 0; i < N; i++)
+		x[i] += 3.22f * z[i];
+
+
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			test[i] += alpha * A[i][j] * x[j] + beta;
+
+
+	for (j = 0; j < N; j++) {
+		if (equal(w[j], test[j]) == 1) {
+			printf("\n %f %f", test[j], w[j]);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+unsigned short int equal(float const a, float const b) {
+
+	if (fabs(a - b) / fabs(a) < EPSILON)
+		return 0; //success
+	else
+		return 1;
+}
+
+
